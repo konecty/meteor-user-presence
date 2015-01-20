34ba3bcb57f4506dade8569f89796b2d360ace7e
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2015-01-20 19:54:07 -0200

    Add package nooitaf:colors

--- package.js
+++ package.js
@@ -8,6 +8,8 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.0.2.1');
 
+  api.use('nooitaf:colors@0.0.2');
+
   api.addFiles('server/server.js', ['server']);
   api.addFiles('server/monitor.js', ['server']);
   api.addFiles('client/client.js', ['client']);
