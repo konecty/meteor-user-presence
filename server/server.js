a31994d09ca747fd9fefa764f9d909a5e9af7ce1
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2015-01-20 19:53:48 -0200

    Now is required UserPresence.start()

--- server/server.js
+++ server/server.js
@@ -1,6 +1,27 @@
 var allowedStatus = ['online', 'away', 'busy', 'offline'];
 
+var logEnable = false;
+
+var log = function(msg, color) {
+	if (logEnable) {
+		if (color) {
+			console.log(msg[color]);
+		} else {
+			console.log(msg);
+		}
+	}
+};
+
+var logRed    = function() {log(Array.prototype.slice.call(arguments).join(' '), 'red');};
+var logGray   = function() {log(Array.prototype.slice.call(arguments).join(' '), 'gray');};
+var logGreen  = function() {log(Array.prototype.slice.call(arguments).join(' '), 'green');};
+var logYellow = function() {log(Array.prototype.slice.call(arguments).join(' '), 'yellow');};
+
 UserPresence = {
+	activeLogs: function() {
+		logEnable = true;
+	},
+
 	removeLostConnections: function() {
 		if (Package['konecty:multiple-instances-status']) {
 			var ids = InstanceStatus.getCollection().find({}, {fields: {_id: 1}}).fetch();
@@ -26,7 +47,7 @@ UserPresence = {
 	},
 
 	removeConnectionsByInstanceId: function(instanceId) {
-		console.log('removeConnectionsByInstanceId', instanceId);
+		logRed('[user-presence] removeConnectionsByInstanceId', instanceId);
 		var update = {
 			$pull: {
 				connections: {
@@ -39,7 +60,7 @@ UserPresence = {
 	},
 
 	removeAllConnections: function() {
-		console.log('removeAllConnections');
+		logRed('[user-presence] removeAllConnections');
 		UsersSessions.remove({});
 	},
 
@@ -56,7 +77,7 @@ UserPresence = {
 			return;
 		};
 
-		console.log('createConnection', userId, connection.id);
+		logGreen('[user-presence] createConnection', userId, connection.id);
 
 		var query = {
 			_id: userId
@@ -89,7 +110,7 @@ UserPresence = {
 			return;
 		};
 
-		console.log('setConnection', userId, connection.id, status);
+		logGray('[user-presence] setConnection', userId, connection.id, status);
 
 		var query = {
 			_id: userId,
@@ -116,13 +137,13 @@ UserPresence = {
 			return;
 		};
 
-		console.log('setDefaultStatus', userId, status);
+		logYellow('[user-presence] setDefaultStatus', userId, status);
 
 		Meteor.users.update({_id: userId, statusDefault: {$ne: status}}, {$set: {statusDefault: status}});
 	},
 
 	removeConnection: function(connectionId) {
-		console.log('removeConnection', connectionId);
+		logRed('[user-presence] removeConnection', connectionId);
 
 		var query = {
 			'connections.id': connectionId
@@ -137,56 +158,55 @@ UserPresence = {
 		};
 
 		UsersSessions.update(query, update);
+	},
+
+	start: function() {
+		Meteor.onConnection(function(connection) {
+			connection.onClose(function() {
+				UserPresence.removeConnection(connection.id);
+			});
+		});
+
+		process.on('exit', function() {
+			if (Package['konecty:multiple-instances-status']) {
+				UserPresence.removeConnectionsByInstanceId(InstanceStatus.id());
+			} else {
+				UserPresence.removeAllConnections();
+			}
+		});
+
+		Accounts.onLogin(function(login) {
+			UserPresence.createConnection(login.user._id, login.connection);
+		});
+
+		Meteor.publish(null, function() {
+			if (this.userId == null) {
+				UserPresence.removeConnection(this.connection.id);
+			}
+		});
+
+		if (Package['konecty:multiple-instances-status']) {
+			UserPresence.startObserveForDeletedServers();
+		}
+
+		UserPresence.removeLostConnections();
+
+		Meteor.methods({
+			'UserPresence:connect': function() {
+				UserPresence.createConnection(this.userId, this.connection);
+			},
+
+			'UserPresence:away': function() {
+				UserPresence.setConnection(this.userId, this.connection, 'away');
+			},
+
+			'UserPresence:online': function() {
+				UserPresence.setConnection(this.userId, this.connection, 'online');
+			},
+
+			'UserPresence:setDefaultStatus': function(status) {
+				UserPresence.setDefaultStatus(this.userId, status);
+			}
+		});
 	}
 }
-
-Meteor.onConnection(function(connection) {
-	connection.onClose(function() {
-		UserPresence.removeConnection(connection.id);
-	});
-});
-
-process.on('exit', function() {
-	if (Package['konecty:multiple-instances-status']) {
-		UserPresence.removeConnectionsByInstanceId(InstanceStatus.id());
-	} else {
-		UserPresence.removeAllConnections();
-	}
-});
-
-
-Meteor.startup(function() {
-	Accounts.onLogin(function(login) {
-		UserPresence.createConnection(login.user._id, login.connection);
-	});
-
-	Meteor.publish(null, function() {
-		if (this.userId == null) {
-			UserPresence.removeConnection(this.connection.id);
-		}
-	});
-
-	if (Package['konecty:multiple-instances-status']) {
-		UserPresence.startObserveForDeletedServers();
-	}
-
-	UserPresence.removeLostConnections();
-
-	Meteor.methods({
-		'UserPresence:connect': function() {
-			UserPresence.createConnection(this.userId, this.connection);
-		},
-
-		'UserPresence:away': function() {
-			UserPresence.setConnection(this.userId, this.connection, 'away');
-		},
-
-		'UserPresence:online': function() {
-			UserPresence.setConnection(this.userId, this.connection, 'online');
-		},
-
-		'UserPresence:setDefaultStatus': function(status) {
-			UserPresence.setDefaultStatus(this.userId, status);
-		}
-	});
-});
