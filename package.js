51cab1bbf6c7376147d4dc94b6e67a91dee2334b
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2015-01-20 13:43:05 -0200

    Update version to 1.0.3

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'konecty:user-presence',
   summary: 'Track user status',
-  version: '1.0.2',
+  version: '1.0.3',
   git: 'https://github.com/Konecty/meteor-user-presence'
 });
 
