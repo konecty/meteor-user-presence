ccbd57f7fe16fe4d8b1cb3f3c91617486603668d
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2015-02-03 12:08:01 -0200

    Prevent error when no user was found in setUserStatus

--- server/monitor.js
+++ server/monitor.js
@@ -20,7 +20,7 @@ UserPresenceMonitor = {
 				UserPresenceMonitor.setUserStatus(record._id, 'offline');
 			}
 			return;
-		}
+		};
 
 		var connectionStatus = 'offline';
 		record.connections.forEach(function(connection) {
@@ -28,32 +28,35 @@ UserPresenceMonitor = {
 				connectionStatus = 'online';
 			} else if (connection.status === 'away' && connectionStatus === 'offline') {
 				connectionStatus = 'away';
-			}
+			};
 		});
 
 		if (record.visitor === true) {
 			UserPresenceMonitor.setVisitorStatus(record._id, connectionStatus);
 		} else {
 			UserPresenceMonitor.setUserStatus(record._id, connectionStatus);
-		}
+		};
 	},
 
 	processUser: function(record) {
-
 		var userSession = UsersSessions.findOne({_id: record._id});
 
 		if (userSession) {
 			UserPresenceMonitor.processUserSession(userSession);
-		}
+		};
 	},
 
 	setUserStatus: function(userId, status) {
-		var user = Meteor.users.findOne(userId)
+		var user = Meteor.users.findOne(userId),
 			statusConnection = status;
+
+		if (!user) {
+			return;
+		};
 
 		if (user.statusDefault != null && status !== 'offline' && user.statusDefault !== 'online') {
 			status = user.statusDefault;
-		}
+		};
 
 		var query = {
 			_id: userId,
