ccded238965a8fa379a6442b7627dc089c5a007f
Elie Steinbock <eliesteinbock@gmail.com>
2015-07-23 12:23:44 +0300

    added this.ready to publication

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'konecty:user-presence',
   summary: 'Track user status',
-  version: '1.2.2',
+  version: '1.2.3',
   git: 'https://github.com/Konecty/meteor-user-presence'
 });
 
