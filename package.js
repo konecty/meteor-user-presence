549378e3f5c9964ca38d9c8d3d21c3bbc3eb5843
Diego Sampaio <chinello@gmail.com>
2020-03-04 19:59:21 -0300

    Bump version to 2.6.2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name: 'konecty:user-presence',
 	summary: 'Track user status',
-	version: '2.6.1',
+	version: '2.6.2',
 	git: 'https://github.com/Konecty/meteor-user-presence'
 });
 
